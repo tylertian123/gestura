6d56367bcaf82ce7803daff0cbbdadc7c592f548
Tyler Tian <tylertian123@gmail.com>
2024-03-19 13:25:23 -0400

    Add error handling for GPIO

--- main/gpio.cpp
+++ main/gpio.cpp
@@ -1,5 +1,6 @@
 #include "gpio.hpp"
 #include "driver/gpio.h"
+#include "esp_err.h"
 #include "hal/gpio_types.h"
 #include "soc/gpio_num.h"
 #include <stdint.h>
@@ -16,12 +17,17 @@ namespace hw {
     GPIO::GPIO(uint8_t pin, gpio_mode_t mode) : GPIO(static_cast<gpio_num_t>(pin), mode) {}
 
     void GPIO::reinit(gpio_mode_t mode) {
+        // Never errors
         gpio_reset_pin(num);
-        gpio_set_direction(num, mode);
+        // Errors on invalid GPIO number
+        // We can just abort since this won't happen without a code error
+        ESP_ERROR_CHECK(gpio_set_direction(num, mode));
     }
 
     bool GPIO::operator=(bool val) {
-        gpio_set_level(num, val);
+        // Errors on invalid GPIO number
+        // We can just abort since this won't happen without a code error
+        ESP_ERROR_CHECK(gpio_set_level(num, val));
         return val;
     }
 
