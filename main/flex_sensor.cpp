d344b9a787ba7e9c104f9cddd00a4d47e6dcfc71
Tyler Tian <tylertian123@gmail.com>
2024-04-07 04:14:01 -0400

    Add IIR filter and clean up flex sensor code

--- main/flex_sensor.cpp
+++ main/flex_sensor.cpp
@@ -1,15 +1,19 @@
 #include "flex_sensor.hpp"
+#include "hal/adc_types.h"
 #include "soc/soc_caps.h"
 #include "util.hpp"
 
 #include <assert.h>
 
 #include "esp_adc/adc_continuous.h"
+#include "esp_adc/adc_filter.h"
 #include "esp_err.h"
-#include "sdkconfig.h"
-
 
 namespace hw {
+    FlexSensorArray::~FlexSensorArray() {
+        ESP_ERROR_CHECK(stop());
+    }
+
     esp_err_t FlexSensorArray::init() {
         adc_continuous_handle_cfg_t handle_cfg = {
             // Total pool only stores the latest frame
@@ -26,35 +30,47 @@ namespace hw {
         pattern[0] = {
             // Attn of 6 dB means a range of approx. 2.19 V, matching the circuit's max voltage
             .atten = ADC_ATTEN_DB_6,
-            .channel = CAT(ADC_CHANNEL_, CONFIG_FLEX_SHORT_ADC_CHAN),
-            .unit = CAT(ADC_UNIT_, CONFIG_FLEX_ADC_UNIT),
+            .channel = FLEX_SHORT_CHAN,
+            .unit = FLEX_UNIT,
             // 12 bits
             .bit_width = SOC_ADC_DIGI_MAX_BITWIDTH,
 
         };
         pattern[1] = {
             .atten = ADC_ATTEN_DB_6,
-            .channel = CAT(ADC_CHANNEL_, CONFIG_FLEX_LONG_ADC_CHAN),
-            .unit = CAT(ADC_UNIT_, CONFIG_FLEX_ADC_UNIT),
+            .channel = FLEX_LONG_CHAN,
+            .unit = FLEX_UNIT,
             .bit_width = SOC_ADC_DIGI_MAX_BITWIDTH,
         };
         adc_continuous_config_t ch_cfg = {
             .pattern_num = 2,
             .adc_pattern = pattern,
-            // Lowest possible frequency for demo
+            // Lowest possible frequency is enough
             .sample_freq_hz = SOC_ADC_SAMPLE_FREQ_THRES_LOW,
-            .conv_mode = CAT(ADC_CONV_SINGLE_UNIT_, CONFIG_FLEX_ADC_UNIT),
+            .conv_mode = FLEX_CONV_MODE,
             // Type2 seems to be the only one that's valid for ESP32-C6
             .format = ADC_DIGI_OUTPUT_FORMAT_TYPE2,
         };
 
         RETURN_ON_ERR(adc_continuous_config(adc_handle, &ch_cfg));
+
+        adc_continuous_iir_filter_config_t filt_cfg = {
+            .unit = FLEX_UNIT,
+            .channel = FLEX_SHORT_CHAN,
+            .coeff = ADC_DIGI_IIR_FILTER_COEFF_16,
+        };
+        RETURN_ON_ERR(adc_new_continuous_iir_filter(adc_handle, &filt_cfg, &filt_short));
+        filt_cfg.channel = FLEX_LONG_CHAN;
+        RETURN_ON_ERR(adc_new_continuous_iir_filter(adc_handle, &filt_cfg, &filt_long));
+
         RETURN_ON_ERR(adc_continuous_start(adc_handle));
         return ESP_OK;
     }
 
     esp_err_t FlexSensorArray::stop() {
         RETURN_ON_ERR(adc_continuous_stop(adc_handle));
+        RETURN_ON_ERR(adc_del_continuous_iir_filter(filt_short));
+        RETURN_ON_ERR(adc_del_continuous_iir_filter(filt_long));
         RETURN_ON_ERR(adc_continuous_deinit(adc_handle));
         return ESP_OK;
     }
@@ -68,13 +84,11 @@ namespace hw {
         
         adc_digi_output_data_t *convs = reinterpret_cast<adc_digi_output_data_t*>(&buf);
         // One of these should always be the short channel and the other the long channel
-        if (convs[0].type2.channel < SOC_ADC_CHANNEL_NUM(CAT(ADC_UNIT_, CONFIG_FLEX_ADC_UNIT))) {
-            (convs[0].type2.channel == CAT(ADC_CHANNEL_, CONFIG_FLEX_LONG_ADC_CHAN) ?
-                long_raw : short_raw) = convs[0].type2.data;
+        if (convs[0].type2.channel < SOC_ADC_CHANNEL_NUM(FLEX_UNIT)) {
+            (convs[0].type2.channel == FLEX_LONG_CHAN ? long_raw : short_raw) = convs[0].type2.data;
         }
-        if (convs[1].type2.channel < SOC_ADC_CHANNEL_NUM(CAT(ADC_UNIT_, CONFIG_FLEX_ADC_UNIT))) {
-            (convs[1].type2.channel == CAT(ADC_CHANNEL_, CONFIG_FLEX_LONG_ADC_CHAN) ?
-                long_raw : short_raw) = convs[1].type2.data;
+        if (convs[1].type2.channel < SOC_ADC_CHANNEL_NUM(FLEX_UNIT)) {
+            (convs[1].type2.channel == FLEX_LONG_CHAN ? long_raw : short_raw) = convs[1].type2.data;
         }
         return ESP_OK;
     }
--- main/flex_sensor.hpp
+++ main/flex_sensor.hpp
@@ -3,15 +3,28 @@
 #include <stdint.h>
 
 #include "esp_adc/adc_continuous.h"
+#include "esp_adc/adc_filter.h"
 #include "esp_err.h"
+
+#include "sdkconfig.h"
+
+#include "util.hpp"
+
+#define FLEX_SHORT_CHAN CAT(ADC_CHANNEL_, CONFIG_FLEX_SHORT_ADC_CHAN)
+#define FLEX_LONG_CHAN CAT(ADC_CHANNEL_, CONFIG_FLEX_LONG_ADC_CHAN)
+#define FLEX_UNIT CAT(ADC_UNIT_, CONFIG_FLEX_ADC_UNIT)
+#define FLEX_CONV_MODE CAT(ADC_CONV_SINGLE_UNIT_, CONFIG_FLEX_ADC_UNIT)
 
 namespace hw {
     class FlexSensorArray {
     private:
         adc_continuous_handle_t adc_handle = {0};
+        adc_iir_filter_handle_t filt_long = {0};
+        adc_iir_filter_handle_t filt_short = {0};
 
     public:
         FlexSensorArray() {};
+        ~FlexSensorArray();
 
         int16_t short_raw = -1;
         int16_t long_raw = -1;
