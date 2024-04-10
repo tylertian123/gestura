be434ac117a7bdd012a28ec3c69b6f18ddbbf59e
pateltanmayp <pateltanmayp@gmail.com>
2024-04-09 21:25:36 -0400

    Add error checking for gesture transmission

--- main/main.cpp
+++ main/main.cpp
@@ -43,23 +43,6 @@ extern "C" void app_main(void) {
 
     ESP_LOGI(TAG, "Started flex sensors");
 
-    flex_sensor_array.read_raw();
-    flex_sensor_array.bucket_values();
-    flex_sensor_array.get_gesture();
-
-    int64_t cur_time = esp_timer_get_time();
-    if (cur_time - last_send > REPORT_PERIOD && flex_sensor_array.change_status == true) {
-        last_send = cur_time;
-        io::Message msg = {
-            .type = io::Message::Type::GESTURE,
-            .gesture = flex_sensor_array.gesture;
-        };
-        if (xQueueSend(self->queue, &msg, 0) != pdTRUE) {
-            //ESP_LOGW(TAG, "Could not send pose: queue full");
-        }
-        flex_sensor_array.change_status = false;
-    }
-
     // if (!pose_int.calibrate_imu()) {
     //     ESP_LOGE(TAG, "Calibration failed");
     // }
@@ -76,6 +59,22 @@ extern "C" void app_main(void) {
     while (true) {
         // Do nothing here for now
         // FreeRTOS will context switch to the pose integrator task for us
-        DELAY_MS(1000);
+        ESP_ERROR_CHECK(flex_sensor_array.read_raw());
+        ESP_ERROR_CHECK(flex_sensor_array.bucket_values());
+        ESP_ERROR_CHECK(flex_sensor_array.get_gesture());
+
+        int64_t cur_time = esp_timer_get_time();
+        if (cur_time - last_send > REPORT_PERIOD && flex_sensor_array.change_status == true) {
+            last_send = cur_time;
+            io::Message msg = {
+                .type = io::Message::Type::GESTURE,
+                .gesture = flex_sensor_array.gesture;
+            };
+            if (xQueueSend(self->queue, &msg, 0) != pdTRUE) {
+                //ESP_LOGW(TAG, "Could not send pose: queue full");
+            }
+            flex_sensor_array.change_status = false;
+        }
+        DELAY_MS(100);
     }
 }
